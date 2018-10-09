fb5cfa751b326665c75393639a874b03c933cb10
beaulieudev <beaulieudev@gmail.com>
2018-10-09 00:22:49 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -4,7 +4,8 @@ def countdown_with_sleep = sleep 1
 def countdown
   while counter > 0
     puts "#{number} SECOND(S)!"
-    counter -= 1
+    countdown_with_sleep -= 1
+    
   end
   puts "HAPPY NEW YEAR!"
 end
