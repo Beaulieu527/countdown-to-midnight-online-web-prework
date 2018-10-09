084b5228996eb49538e794a7da1c736112e203d3
beaulieudev <beaulieudev@gmail.com>
2018-10-09 00:23:27 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,11 @@
 #write your code here
-
+counter = 5
+def countdown_with_sleep = sleep 1
 def countdown
+  while counter > 0
+    puts "#{number} SECOND(S)!"
+    countdown_with_sleep -= 1
+
+  end
+  puts "HAPPY NEW YEAR!"
 end
